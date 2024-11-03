bd920d3baccbf98ffb2ba2573acdcaae4da12290
Dzmitry <40037467+AmbushedRaccoon@users.noreply.github.com>
2024-11-03 21:07:19 +0200

    Added support for androif builds using capacitor
    Added capacitor.
    readme modified.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.petapp.fe',
+  appName: 'PetApp',
+  webDir: 'dist/pet-app/browser'
+};
+
+export default config;
