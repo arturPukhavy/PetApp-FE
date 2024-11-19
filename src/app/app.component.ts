c288a182da5e15acfa585ed89e0ef92253562a07
Artur Pukhavoi <arturpuhice@gmail.com>
2024-11-19 23:13:52 +0300

    add ionic features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,16 +5,23 @@ import { FiltersComponent } from "./pages/filters/filters.component";
 import { PetDetailsComponent } from "./pages/pet-details/pet-details.component";
 import { ProfileComponent } from "./pages/profile/profile.component";
 import { SearchResultsPageComponent } from "./pages/search-results-page/search-results-page.component";
-import { IonAccordion, IonAccordionGroup, IonicModule } from '@ionic/angular';
-import { IonApp } from '@ionic/angular/standalone';
+import { IonAccordion, IonAccordionGroup, IonicModule, Platform } from '@ionic/angular';
+
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ HeaderComponent, FiltersComponent, SearchResultsPageComponent,IonicModule],
+  imports: [ HeaderComponent, FiltersComponent, SearchResultsPageComponent,IonicModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'PetApp';
+  isNative: boolean;
+
+  constructor(private platform: Platform) {
+    this.isNative = this.platform.is('capacitor');
+  }
 }
